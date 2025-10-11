24dc722da03853542b1ac53d82674d0063c769b7
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2025-10-11 14:37:17 -0700

    Update googleGeminiChatModelDemo.py

--- 01_LangChainModels/ChatModels/googleGeminiChatModelDemo.py
+++ 01_LangChainModels/ChatModels/googleGeminiChatModelDemo.py
@@ -1,1 +1,11 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+load_dotenv()
+
+print("Started")
+
+
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+result = model.invoke("Tell me a poem of 5 lines on Football")
+print(result.content)
 
